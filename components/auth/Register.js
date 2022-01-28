d3bfa4f27dbc357f5db8aaae6da1e35174a8b01d
Anuja Deshpande <anuja.deshpande@spherity.com>
2022-01-28 16:27:14 +0100

    Authantication added

---
+++ components/auth/Register.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput } from 'react-native'
+import firebaseConfig from '../../config';
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    createUserWithEmailAndPassword
+  } from 'firebase/auth';
+
+  initializeApp(firebaseConfig);
+  const auth = getAuth();
+
+export class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }
+
+        this.onSignUp = this.onSignUp.bind(this)
+    }
+
+    onSignUp() {
+        const { email, password, name } = this.state;
+       createUserWithEmailAndPassword(auth,email, password)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder="name"
+                    onChangeText={(name) => this.setState({ name })}
+                />
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
+
+                <Button
+                    onPress={() => this.onSignUp()}
+                    title="Sign Up"
+                />
+            </View>
+        )
+    }
+}
+
+export default Register
